6e908cb5fa6596c663156925926d8b7df8d6b28d
Jason Ren <renjiawei1997@gmail.com>
2019-03-19 16:05:22 +0800

    train/val split

--- utils/data/dataset.py
+++ utils/data/dataset.py
@@ -37,8 +37,9 @@ class Dataset(object):
         if num_val >= num or num_val < 0:
             raise ValueError("num_val exceeds total identities {}"
                              .format(num))
-        self.val = self.train_val[:-num_val]
-        self.train = self.train_val[-num_val:]
+        self.val = self.train_val[:-num_val].tolist()
+        self.train = self.train_val[-num_val:].tolist()
+        self.train_val = self.train_val.tolist()
 
 
         self.test = self.file2_tuple_list(osp.join(self.split_path, 'testlist0' + str(self.split_id) + '.txt'))
