1a7089257381ada4da8fae01ffa4e994849dc0f4
Jason Ren <renjiawei1997@gmail.com>
2019-03-19 15:49:10 +0800

    train/val split

--- utils/data/dataset.py
+++ utils/data/dataset.py
@@ -1,11 +1,11 @@
 import os.path as osp
-
+import numpy as np
 
 class Dataset(object):
     def __init__(self,root, split_id):
         self.root = root
         self.split_id = split_id
-        self.train, self.val = [], []
+        self.train, self.val, self.train_val, self.test= [], [], [], []
         self.num_train_ids, self.num_val_ids = 0, 0
         self.num_class = 0
         self.material_label = {}
@@ -24,13 +24,28 @@ class Dataset(object):
         self.num_class = len(self.material_label)
 
 
-    def load(self):
+    def load(self, num_val=0.3):
         self.get_class_index()
-        self.train = self.file2_tuple_list(osp.join(self.split_path, 'trainlist0' + str(self.split_id) + '.txt'))
-        self.val = self.file2_tuple_list(osp.join(self.split_path, 'testlist0' + str(self.split_id) + '.txt'))
+        self.train_val = self.file2_tuple_list(osp.join(self.split_path, 'trainlist0' + str(self.split_id) + '.txt'))
+
+        # Randomly split train / val
+        self.train_val = np.asarray(self.train_val)
+        np.random.shuffle(self.train_val)
+        num = len(self.train_val)
+        if isinstance(num_val, float):
+            num_val = int(round(num * num_val))
+        if num_val >= num or num_val < 0:
+            raise ValueError("num_val exceeds total identities {}"
+                             .format(num))
+        self.val = sorted(self.train_val[:-num_val])
+        self.train = sorted(self.train_val[-num_val:])
+
+
+        self.test = self.file2_tuple_list(osp.join(self.split_path, 'testlist0' + str(self.split_id) + '.txt'))
         self.num_train_ids = len(self.train)
         self.num_val_ids = len(self.val)
-        print ('==> (Training images, Validation images):(', self.num_train_ids,self.num_val_ids ,')')
+        self.num_test_ids = len(self.val)
+        print ('==> (Training images, Validation images, Test images):(', self.num_train_ids,self.num_val_ids , self.num_test_ids,')')
         return self.train, self.val
 
     @property
@@ -48,5 +63,3 @@ class Dataset(object):
             label = int(self.material_label[line.split('/')[0]])-1
             tl.append((key,label))
         return tl
-
-
