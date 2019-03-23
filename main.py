ff5b8c8e0b2ff9a8454c4940dc3f37a4d3fedfa8
Jason Ren <renjiawei1997@gmail.com>
2019-03-23 21:22:22 +0800

    remove embedding

--- main.py
+++ main.py
@@ -185,14 +185,12 @@ def main(args):
     img_param_groups = [
         {'params': img_branch.module.low_level_modules.parameters(), 'lr_mult': 0.1},
         {'params': img_branch.module.high_level_modules.parameters(), 'lr_mult': 0.1},
-        {'params': img_branch.module.feat.parameters(), 'lr_mult': 1},
         {'params': img_branch.module.classifier.parameters(), 'lr_mult': 1},
     ]
 
     diff_param_groups = [
         {'params': diff_branch.module.low_level_modules.parameters(), 'lr_mult': 0.1},
         {'params': diff_branch.module.high_level_modules.parameters(), 'lr_mult': 0.1},
-        {'params': diff_branch.module.feat.parameters(), 'lr_mult': 1},
         {'params': diff_branch.module.classifier.parameters(), 'lr_mult': 1},
     ]
 
